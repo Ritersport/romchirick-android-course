ad775f6b38176592df1c18f1d1eba580d3c59e0e
Ritersport <58008120+Ritersport@users.noreply.github.com>
2023-10-28 12:03:59 +0700

    Delete app directory

--- app/src/main/java/nsu/leorita/exchanges/domain/model/RangesInfo.kt
+++
@@ -1,6 +0,0 @@
-package nsu.leorita.exchanges.domain.model
-
-data class RangesInfo(
-    val date: String,
-    val ranges: List<Currency>
-)
--- app/src/main/java/nsu/leorita/exchanges/domain/services/RangesService.kt
+++
@@ -1,8 +0,0 @@
-package nsu.leorita.exchanges.domain.services
-
-import io.reactivex.Single
-import nsu.leorita.exchanges.domain.model.RangesInfo
-
-interface RangesService {
-    fun getRanges(): Single<RangesInfo>
-}
